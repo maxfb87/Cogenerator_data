197f697120169dff42c482682dfc1e38259a1ab7
maxfb87 <maxfb87@yahoo.it>
2021-09-13 22:27:59 +0200

    Version with cost and revenue dictionaries
    This version of function is with dictionaries instead of variables to describe costs and revenues.

--- get_cogen_saving.py
+++ get_cogen_saving.py
@@ -54,40 +54,43 @@ def get_cogen_saving(cogen_functioning_regime, requested_electric_power, request
     input_power = electric_power / electric_efficiency
 
     thermal_power = input_power * thermal_efficiency
-    
-    #Costs
-    ch4_purchase_cost = input_power * CH4_PRICE / HEATING_CH4_POWER
-    maintenance_cost = electric_power * MAINTENANCE_COGENERATOR_PRICE
-    tax_cost = electric_power * TAX_PRICE
-    distribution_pump_cost = DISTRIBUTION_PUMP_POWER * ELECTRIC_ENERGY_PRICE
+        
+    costs = collections.defaultdict(dict)
+    revenues = collections.defaultdict(dict)
 
-    #Revenues and savings
+    costs["no cogen"]["ch4"] = requested_thermal_power * EFFECTIVE_KWH_COST
+    costs["no cogen"]["electric energy"] = requested_electric_power * ELECTRIC_ENERGY_PRICE
+    costs["no cogen"]["classic water heater electric energy"] = classic_water_heater_electric_power * ELECTRIC_ENERGY_PRICE
+
+    costs["cogen"]["ch4"] = input_power * CH4_PRICE / HEATING_CH4_POWER
+
+    if requested_thermal_power > thermal_power:
+        costs["cogen"]["ch4"] += (requested_thermal_power-thermal_power) * EFFECTIVE_KWH_COST
+    
+    costs["cogen"]["maintainance"] = electric_power * MAINTENANCE_COGENERATOR_PRICE
+    costs["cogen"]["tax"] = electric_power * TAX_PRICE
+    costs["cogen"]["distribution pump"] = DISTRIBUTION_PUMP_POWER * ELECTRIC_ENERGY_PRICE
+    costs["cogen"]["classic water heater electric energy"] = (1 - thermal_power / classic_water_heater_thermal_power) * classic_water_heater_electric_power * ELECTRIC_ENERGY_PRICE
+
+    if requested_electric_power > electric_power:
+        costs["cogen"]["electric energy"] = (requested_electric_power-electric_power) * ELECTRIC_ENERGY_PRICE
+    
     if requested_electric_power < electric_power:
-        electric_energy_revenue = (electric_power - requested_electric_power) * ELECTRIC_ENERGY_SOLD_PRICE
-        electric_energy_saving = requested_electric_power * ELECTRIC_ENERGY_PRICE
+        revenues["cogen"]["electric energy"] = (electric_power - requested_electric_power) * ELECTRIC_ENERGY_SOLD_PRICE
     else:
-        electric_energy_revenue = 0
-        electric_energy_saving = electric_power * ELECTRIC_ENERGY_PRICE
+        revenues["cogen"]["electric energy"] = 0
 
-    if requested_thermal_power < thermal_power:
-        thermal_energy_saving = requested_thermal_power * EFFECTIVE_KWH_COST
-    else:
-        thermal_energy_saving = thermal_power * EFFECTIVE_KWH_COST
-    
-    classic_heating_water_electric_saving = thermal_power / classic_water_heater_thermal_power * classic_water_heater_electric_power * ELECTRIC_ENERGY_PRICE
-    government_revenue = GOVERNMENT_BENEFIT * electric_power
-    
+    revenues["cogen"]["government"] = GOVERNMENT_BENEFIT * electric_power
 
-    total_costs = ch4_purchase_cost + maintenance_cost + tax_cost + distribution_pump_cost
-    total_savings = electric_energy_saving + thermal_energy_saving + classic_heating_water_electric_saving
-    total_revenues = electric_energy_revenue + government_revenue
+    no_cogen_total_costs = sum(costs["no cogen"].values())
+    cogen_total_costs = sum(costs["cogen"].values())
+    cogen_total_revenues = sum(revenues["cogen"].values())
 
-    print(f"{total_costs} - {total_savings} - {total_revenues}")
-
-    print(f"{total_costs} - {total_savings+total_revenues}")
-
-    print(f"BALANCE: {total_savings+total_revenues-total_costs} \n")
-
+    print(f"TOTAL NO COGEN COSTS: {no_cogen_total_costs} €")
+    print(f"++ TOTAL COGEN COSTS: {cogen_total_costs} €")
+    print(f"TOTAL COGEN REVENUE: {cogen_total_revenues} €")
+    print(f"++ TOTAL COGEN COSTS-REVENUES: {cogen_total_costs - cogen_total_revenues} €")
+    print(f"BALANCE:  {no_cogen_total_costs+cogen_total_revenues-cogen_total_costs}")
 
 # If this program was run (instead of imported), run the script:
 if __name__ == "__main__":
