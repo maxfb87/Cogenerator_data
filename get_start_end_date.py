d628d27c95a9f85f6ab94d872d769cb394b657a3
maxfb87 <maxfb87@yahoo.it>
2021-10-03 19:05:13 +0200

    Create get_start_end_date.py

---
+++ get_start_end_date.py
@@ -0,0 +1,30 @@
+import calendar as calendar
+import datetime
+
+def get_start_end_date(year, month):
+    """This function returns the first day and the last day, of a month, in string format.
+    Date is returned in english format (yyyy-mm-dd)
+    Input values:
+    - year: string
+    - month: string from 1 to 12
+
+    Output values:
+    - start_date: str in yyyy-mm-dd format
+    - end_date: str in yyyy-mm-dd format
+    """
+
+    try:
+        datetime.datetime(int(year), 10, 30)
+    except ValueError:
+        raise("Year variable has something wrong. Please check it")
+
+    try:
+        datetime.datetime(2000, int(month), 30)
+    except ValueError:
+        raise("Month variable has something wrong. Please check it")
+
+    start_date = year + "-" + month + "-" + str('01')
+    end_date = year + "-" + month + "-" + str(calendar.monthrange(int(year),int(month))[1])
+
+    return start_date, end_date
+
