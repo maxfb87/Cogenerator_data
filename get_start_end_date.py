c4422688a5b459951da4c27270435bc7b73c88c7
maxfb87 <maxfb87@yahoo.it>
2022-02-01 22:48:40 +0100

    add check on month variable
    FIrst char has to be 0

--- get_start_end_date.py
+++ get_start_end_date.py
@@ -19,6 +19,9 @@ def get_start_end_date(year, month):
         raise("Year variable has something wrong. Please check it")
 
     try:
+        print(f"Ecco qui {month[0]}")
+        if (int(month) < 10) and (int(month[0]) != 0):
+            month = "0" + month
         datetime.datetime(2000, int(month), 30)
     except ValueError:
         raise("Month variable has something wrong. Please check it")
